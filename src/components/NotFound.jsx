dcffa803b5bd76a80553295e5a317781b79d945f
jzamora5 <jzamora_5@yahoo.com>
2021-04-09 20:57:19 -0500

    Base Project

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../assets/styles/components/NotFound.css";
+
+const NotFound = () => {
+  return <div className="not-found">Not Found</div>;
+};
+
+export default NotFound;
---
+++ src/containers/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../assets/styles/Home.css";
+
+const Home = () => {
+  return <div className="home">Hello There</div>;
+};
+
+export default Home;
