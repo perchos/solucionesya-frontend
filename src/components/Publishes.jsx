7f13603a1be5e7f0f627344692523f92a7aff7dc
1270 <1270@holbertonschool.com>
2021-04-24 15:44:25 -0500

    [fix] form styles

--- src/components/Publishes.jsx
+++ src/components/Publishes.jsx
@@ -10,8 +10,12 @@ import '../assets/styles/Publishes.css';
 import React from 'react';
 
 const Publishes = () => {
+  const handleSubmit = () => {
+    console.log('working in handle submit ... ');
+  };
+
   return (
-    <Container className='publishes'>
+    <Container className='publishes py-5'>
       <h2>Publicar Mi Servicio</h2>
       <Row>
         <Col>
@@ -33,17 +37,25 @@ const Publishes = () => {
                 name='hourly'
               />
             </Form.Group>
-            <Form.Group controlId='location'>
-              <Form.Control as='select' defaultValue='Ubicación...'>
-                <option>Ubicación...</option>
-                <option>...</option>
-              </Form.Control>
-            </Form.Group>
-            <FormControl
-              type='number'
-              placeholder='Teléfono'
-              className='mb-2'
-            />
+            <Form.Row>
+              <Col>
+                <Form.Group controlId='location'>
+                  <Form.Control as='select' defaultValue='Ubicación...'>
+                    <option>Ubicación...</option>
+                    <option>...</option>
+                  </Form.Control>
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId='location'>
+                  <FormControl
+                    type='number'
+                    placeholder='Teléfono'
+                    className='mb-2'
+                  />
+                </Form.Group>
+              </Col>
+            </Form.Row>
             <Form.Group controlId='description'>
               <Form.Control
                 as='textarea'
@@ -63,7 +75,7 @@ const Publishes = () => {
               className='custom-file'
             />
           </Form>
-          <Button variant='primary' className='my-2'>
+          <Button variant='primary' className='my-2' onClick={handleSubmit}>
             Publicar
           </Button>
         </Col>
