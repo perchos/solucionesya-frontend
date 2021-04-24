3107f8e381b70a5e77fc43ddeea5d57a6026e285
Michael Cocuy <maicol.asus@gmail.com>
2021-04-24 00:32:54 -0500

    [add] develop is ready

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,27 +1,65 @@
-import React from "react";
-import { Card, Form, Button } from 'react-bootstrap';
+import React from 'react'
+import '../assets/styles/Login.css'
 
 const Login = () => {
-  return (
-    <Card>
-      <Card body>
-        <Card.Title>Iniciar Sesión</Card.Title>
-      </Card>
-      <Form>
-        <Form.Group controlId="formGroupUser">
-          <Form.Label>Usuario</Form.Label>
-          <Form.Control type="text" />
-        </Form.Group>
-        <Form.Group controlId="formGroupPassword">
-          <Form.Label>Contraseña</Form.Label>
-          <Form.Control type="password" />
-        </Form.Group>
-        <Card.Link href="#">¿Has olvidado tu contraseña?</Card.Link>
-        <Button variant="primary" type="submit">Iniciar Sesión</Button>
-      </Form>
-      <Card.Text>¿No tienes una cuenta? <Card.Link href="#">Registrate</Card.Link></Card.Text>
-    </Card>
-  );
-};
+
+    function loginUser(e){
+      console.log(e.target.userName.value)
+      console.log(e.target.userPassword.value)
+
+      const url = 'http://localhost:5000/auth/login'
+
+      let data = {
+        userName: e.target.userName.value,
+        password: e.target.userPassword.value
+      }
+
+      fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'include',
+        body: JSON.stringify(data),
+        headers:{'Content-Type': 'application/json'}
+      }).then( res => {
+        console.log(res)
+      }).catch(
+        err => {
+            console.log(err)
+        }
+      )
+      e.preventDefault()
+    }
+
+    return <div className="container">
+    <div className="row">
+      <div className="col-lg-10 mx-auto">
+        <div className="card border-0 shadow flex-row my-3">
+          <div className="login-card-img-left rounded-left d-none d-md-flex">
+          </div>
+          <div className="card-body login-spacing-inner">
+            <h5 className="card-title text-center">Iniciar Sesión</h5>
+            <form onSubmit={loginUser}>
+              <div className="mt-1">
+                <label className="mb-0" htmlFor="userName">Usuario</label>
+                <input type="text" id="userName" className="form-control form-control-sm" placeholder="Nombre de Usuario" autoFocus/>
+              </div>
+
+              <div className="mt-1">
+                <label className="mb-0" htmlFor="userPassword">Contraseña</label>
+                <input type="password" id="userPassword" className="form-control form-control-sm" placeholder="Escribe Tu Contraseña"/>
+              </div>
+
+              <button className="btn btn-color btn-block mt-4" type="submit">Iniciar Sesión</button>
+              <p className="d-block text-center mt-3 small" href="">
+                ¿No tienes una cuenta?
+                <a className="d-block text-center mt-2" href="/register">Registrarme</a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  };
 
 export default Login;
--- src/routes/App.js
+++ src/routes/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../components/Home';
 import Login from '../components/Login';
+import Register from '../components/Register'
 import Publishes from '../components/Publishes';
 import NotFound from '../components/NotFound';
 import '../assets/styles/App.css';
@@ -12,6 +13,7 @@ const App = () => {
       <Switch>
         <Route exact path='/' component={Home} />
         <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
         <Route exact path='/publishes' component={Publishes} />
         <Route component={NotFound} />
       </Switch>
